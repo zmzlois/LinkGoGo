9d7e67ee1198067a4361b0dcd69784ba3cbb0f02
zmzlois <lois.z@comcord.vision>
2024-02-27 23:53:05 +0000

    add grant type

--- auth/grant_type.go
+++ auth/grant_type.go
@@ -16,4 +16,19 @@ func (c *Client) implicitGrant() string {
 // use the safer/default response type for getting the users access token
 func (c *Client) explicitGrant() string {
 
+	// establish the prompt parameter
+	var prompt string = c.Prompt
+
+	if len(c.Prompt) > 0 {
+		prompt = "&prompt=" + c.Prompt
+	}
+
+	// return the OAuth URL to a formatted string containing the client id, redirect uri, response type and prompt parameter
+	return fmt.Sprintf(
+		"https://discord.com/api/oauth2/authorize?client_id=%s&redirect_uri=%s&response_type=code%s",
+		c.ClientId,
+		c.RedirectUri,
+		prompt,
+	)
+
 }
