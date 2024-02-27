78ea458a7cb72acf28e96904ba961d3e9ac0593d
zmzlois <lois.z@comcord.vision>
2024-02-27 23:53:23 +0000

    scopes in discord

---
+++ auth/scopes.go
@@ -0,0 +1,28 @@
+package auth
+
+const (
+	// Non-Whitelist
+	ScopeIdentify                   = "identify"
+	ScopeBot                        = "bot"
+	ScopeEmail                      = "email"
+	ScopeGuilds                     = "guilds"
+	ScopeGuildsJoin                 = "guilds.join"
+	ScopeConnections                = "connections"
+	ScopeGroupDMJoin                = "gdm.join"
+	ScopeMessagesRead               = "messages.read"
+	ScopeWebhookIncoming            = "webhook.Incoming"
+	ScopeApplicationsBuildsRead     = "applications.builds.read"
+	ScopeApplicationsStoreUpdate    = "applications.store.update"
+	ScopeApplicationsEntitlements   = "applications.entitlements"
+	ScopeApplicationsCommands       = "applications.commands"
+	ScopeApplicationsCommandsUpdate = "applications.commands.update"
+
+	// Whitelist Only
+	ScopeRPC                      = "rpc"
+	ScopeRPCAPI                   = "rpc.api"
+	ScopeRPCNotificationsRead     = "rpc.notifications.read"
+	ScopeApplicationsBuildsUpload = "applications.builds.upload"
+	ScopeRelationshipsRead        = "relationships.read"
+	ScopeActivitiesRead           = "activities.read"
+	ScopeActivitiesWrite          = "activities.write"
+)
