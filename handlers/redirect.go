9f6b71cef962af7567a86af98e6e544a010df97f
zmzlois <lois.z@comcord.vision>
2024-03-01 12:07:51 +0000

    redirect connect to database

--- handlers/redirect.go
+++ handlers/redirect.go
@@ -1,24 +1,41 @@
 package handlers
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	dsc "github.com/realTristan/disgoauth"
 )
 
-func RedirectHandler(w http.ResponseWriter, r *http.Request, ds *dsc.Client) {
-	code := r.URL.Query()["code"][0]
+func RedirectHandler(ds *dsc.Client) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query()["code"][0]
 
-	token, _ := ds.GetOnlyAccessToken(code)
+		token, _ := ds.GetOnlyAccessToken(code)
 
-	if len(token) > 1 {
+		if len(token) < 5 {
+			authFailed(Realm{authenticate: "Unauthorized"})
+			w.Write([]byte("Something went wrong when we are authenticating you, please come back again later"))
+		}
+		// get user data to store in the database and also to display on the page
+		result, _ := dsc.GetUserData(token)
 
+		avatar := result["avatar"].(string)
+		global_name := result["global_name"].(string)
+
+		fmt.Printf("Avatar: %s\n Global name: %s \n", avatar, global_name)
+
+		fmt.Println(result)
+
+		http.Redirect(w, r, "/edit", http.StatusFound)
 	}
-	// get user data to store in the database and also to display on the page
-	result, _ := dsc.GetUserData(token)
+}
 
-	fmt.Println(result)
+func CreateUser(userData map[string]interface{}) {
 
-	http.Redirect(w, r, "/edit", http.StatusFound)
+	ctx := context.Background()
+	db := DatabaseClient(ctx)
+	defer db.Close()
+
 }
