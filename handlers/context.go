74cc2388cf75f379459adcbe5d6a9e009bfa3de1
zmzlois <lois.z@comcord.vision>
2024-03-01 00:40:53 +0000

    WIP: add request context

---
+++ handlers/context.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"context"
+	"net/http"
+)
+
+type Authenticated bool
+
+var (
+	authenticated Authenticated
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// create new context from `r` request context, and assign key `"user"`
+		// to value of `"123"`
+		ctx := context.WithValue(r.Context(), authenticated, true)
+
+		// call the next handler in the chain, passing the response writer and
+		// the updated request object with the new context value.
+		//
+		// note: context.Context values are nested, so any previously set
+		// values will be accessible as well, and the new `"user"` key
+		// will be accessible from this point forward.
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
