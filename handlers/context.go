625538c86de1069df7bfe9d71f21d73d4b4411fa
zmzlois <lois.z@comcord.vision>
2024-03-01 12:07:40 +0000

    update context

--- handlers/context.go
+++ handlers/context.go
@@ -1,28 +1,59 @@
 package handlers
 
 import (
-	"context"
+	"fmt"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-type Authenticated bool
+type Realm struct {
+	authenticate string `json:"authenticate"`
+}
 
-var (
-	authenticated Authenticated
-)
+type UserContext struct {
+	UserID   string `json:"user_id"`
+	UserName string `json:"username"`
 
-func AuthMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// create new context from `r` request context, and assign key `"user"`
-		// to value of `"123"`
-		ctx := context.WithValue(r.Context(), authenticated, true)
+	// Add other relevant user data fields here
+}
 
-		// call the next handler in the chain, passing the response writer and
-		// the updated request object with the new context value.
-		//
-		// note: context.Context values are nested, so any previously set
-		// values will be accessible as well, and the new `"user"` key
-		// will be accessible from this point forward.
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
+func AuthMiddlewarer(token string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			// if !ok {
+			// 	authFailed(Realm{authenticate: "Unauthorized"})
+			// 	return
+			// }
+
+			// If the user is authenticated, call the next handler
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func authFailed(authenticate Realm) func(next http.Handler) http.Handler {
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("WWW-Authenticate", fmt.Sprintf(`Basic real=%s`, authenticate))
+			w.WriteHeader(http.StatusUnauthorized)
+			http.Redirect(w, r, "/failed", http.StatusUnauthorized)
+		})
+	}
+
+}
+
+func HashValue(v string) string {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(v), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	return string(hashedPassword)
+}
+
+func CompareToHash(v, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(v))
+	return err == nil
 }
