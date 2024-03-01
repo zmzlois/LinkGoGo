67b644728c1940df1e8fb25497641473f0e77e8b
zmzlois <lois.z@comcord.vision>
2024-03-01 00:41:38 +0000

    separate sentry out

--- monitor/sentry.go
+++ monitor/sentry.go
@@ -1,9 +1,15 @@
 package monitor
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/getsentry/sentry-go"
+	sentryhttp "github.com/getsentry/sentry-go/http"
+	"github.com/joho/godotenv"
 )
 
 type handler struct{}
@@ -26,3 +32,44 @@ func enhanceSentryEvent(handler http.HandlerFunc) http.HandlerFunc {
 		handler(rw, r)
 	}
 }
+
+func SentryInit() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	sentryDSN := os.Getenv("SENTRY_DSN")
+
+	if len(sentryDSN) == 0 {
+		panic("SENTRY_DSN is required for sentry to work")
+	}
+
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn: sentryDSN,
+		BeforeSend: func(event *sentry.Event, hint *sentry.EventHint) *sentry.Event {
+			if hint.Context != nil {
+				if req, ok := hint.Context.Value(sentry.RequestContextKey).(*http.Request); ok {
+					fmt.Printf("Request URL: %s", req.URL)
+				}
+			}
+
+			return event
+		},
+		Debug:            true,
+		AttachStacktrace: true,
+		EnableTracing:    true,
+		TracesSampleRate: 1.0,
+	}); err != nil {
+		panic(fmt.Sprintf("[Sentry error]: %s", err))
+	}
+
+}
+
+var sentryHandler = sentryhttp.New(sentryhttp.Options{
+	Repanic:         true,
+	WaitForDelivery: true,
+	Timeout:         2 * time.Second,
+})
