febdac1068fa52088e5cf9d7b7031d5b296ddb3e
zmzlois <lois.z@comcord.vision>
2024-03-07 22:43:07 +0000

    query links

---
+++ pkg/service/link_service.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/a-h/templ"
+	"github.com/zmzlois/LinkGoGo/ent"
+	"github.com/zmzlois/LinkGoGo/ent/links"
+	"github.com/zmzlois/LinkGoGo/ent/users"
+	"github.com/zmzlois/LinkGoGo/pkg/model"
+)
+
+type LinkService struct {
+	db *ent.Client
+}
+
+func NewLinkService(db *ent.Client) *LinkService {
+	return &LinkService{db: db}
+}
+
+func (b *LinkService) GetLinks(ctx context.Context, externalId string) ([]model.NewLinkInput, error) {
+
+	var userLinks = []model.NewLinkInput{}
+
+	task, err := b.db.Links.
+		Query().
+		Where(links.HasUserWith(users.ExternalID(externalId))).
+		Order(ent.Asc(links.FieldOrder)).
+		All(ctx)
+
+	if err != nil {
+		return userLinks, err
+	}
+
+	if len(task) == 0 {
+		return userLinks, nil
+	}
+
+	for _, link := range task {
+
+		safeURL := templ.SafeURL(link.URL)
+
+		userLinks = append(userLinks, model.NewLinkInput{
+			Title:       link.Title,
+			Description: link.Description,
+			Url:         safeURL,
+			Order:       link.Order,
+		})
+		if link.URL == "" {
+			continue
+		}
+	}
+
+	fmt.Println("Userlink", userLinks)
+
+	return userLinks, nil
+}
