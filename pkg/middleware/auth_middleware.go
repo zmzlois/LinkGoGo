d6c78cf8ff6e5661ba820b54c46e4cee857d54a9
zmzlois <lois.z@comcord.vision>
2024-03-08 21:03:15 +0000

    simplify auth middleware

--- pkg/middleware/auth_middleware.go
+++ pkg/middleware/auth_middleware.go
@@ -1,28 +1,20 @@
 package middleware
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
-	"github.com/aidenwallis/go-write/write"
-
 	"github.com/zmzlois/LinkGoGo/pkg/auth"
-	"github.com/zmzlois/LinkGoGo/pkg/model"
 	"go.uber.org/zap"
 )
 
-var PublicRoutes = []string{
-	"/",
-	"/login",
-	"/discord-redirect",
-	"/discord-oauth",
-}
-
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
 		tokenString, err := auth.GetToken(r)
 
-		if err != nil || tokenString == "" || len(tokenString) < 1 {
+		if err != nil || tokenString == "" {
 			fmt.Printf("AuthMiddleware.GetToken: %v", zap.Error(err))
 
 			http.Redirect(w, r, "/unauthorised", http.StatusFound)
@@ -31,22 +23,41 @@ func AuthMiddleware(next http.Handler) http.Handler {
 
 		// Parse and validate JWT token
 		ctx := r.Context()
-		authenticated, err := auth.ValidateToken(tokenString)
+		authenticated, user, userID, err := validateTokenAndGetUser(ctx, tokenString, r)
 		if err != nil {
-
-			fmt.Println("failed to validate session token", zap.Error(err))
-			_ = write.InternalServerError(w).JSON(model.UnknownError)
-
-			http.Redirect(w, r, "/unauthorised", http.StatusFound)
-
+			// Handle token validation error
+			fmt.Println("Failed to validate session token:", err)
+			http.Error(w, "Failed to validate session token", http.StatusInternalServerError)
 			return
 		}
 
 		if authenticated {
-			ctx = auth.WithSession(ctx, "token", tokenString)
+			ctx = setUserInfoInContext(ctx, user, userID)
 		}
-
 		// Token is valid, call next handler
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
+}
+
+func validateTokenAndGetUser(ctx context.Context, tokenString string, r *http.Request) (bool, map[string]interface{}, string, error) {
+	authenticated, err := auth.ValidateToken(tokenString)
+	if err != nil {
+		return false, nil, "", err
+	}
+
+	user, userID, err := auth.RetrieveTokenValue("user_id", r)
+	if err != nil {
+		return false, nil, "", err
+	}
+
+	return authenticated, user, userID.(string), nil
+}
+
+func setUserInfoInContext(ctx context.Context, user map[string]interface{}, userID string) context.Context {
+
+	ctx = auth.WithSession(ctx, "user_id", userID)
+	ctx = auth.WithSession(ctx, "username", user["username"].(string))
+	ctx = auth.WithSession(ctx, "avatar", user["avatar"].(string))
+	ctx = auth.WithSession(ctx, "global_name", user["global_name"].(string))
+	return ctx
 }
